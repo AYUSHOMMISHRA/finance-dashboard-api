754be5f09a616a6673e2bc6ed9529793964c13b6
AYUSH MISHRA <connect.ayushommishra@gmail.com>
2026-04-05 23:04:32 +0530

    db(prisma): add schema.prisma and seed script (create users, records, audit logs)

---
+++ prisma/migrations/20260405135956_ayush/migration.sql
@@ -0,0 +1,84 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('VIEWER', 'ANALYST', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('ACTIVE', 'INACTIVE');
+
+-- CreateEnum
+CREATE TYPE "RecordType" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "status" "Status" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "FinancialRecord" (
+    "id" TEXT NOT NULL,
+    "amount" DECIMAL(12,2) NOT NULL,
+    "type" "RecordType" NOT NULL,
+    "category" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "notes" TEXT,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "createdById" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FinancialRecord_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "AuditLog" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "action" TEXT NOT NULL,
+    "entity" TEXT NOT NULL,
+    "entityId" TEXT,
+    "meta" JSONB,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "AuditLog_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_createdById_idx" ON "FinancialRecord"("createdById");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_type_idx" ON "FinancialRecord"("type");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_category_idx" ON "FinancialRecord"("category");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_date_idx" ON "FinancialRecord"("date");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_isDeleted_idx" ON "FinancialRecord"("isDeleted");
+
+-- CreateIndex
+CREATE INDEX "FinancialRecord_isDeleted_type_date_idx" ON "FinancialRecord"("isDeleted", "type", "date");
+
+-- CreateIndex
+CREATE INDEX "AuditLog_userId_idx" ON "AuditLog"("userId");
+
+-- CreateIndex
+CREATE INDEX "AuditLog_entity_entityId_idx" ON "AuditLog"("entity", "entityId");
+
+-- AddForeignKey
+ALTER TABLE "FinancialRecord" ADD CONSTRAINT "FinancialRecord_createdById_fkey" FOREIGN KEY ("createdById") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "AuditLog" ADD CONSTRAINT "AuditLog_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
